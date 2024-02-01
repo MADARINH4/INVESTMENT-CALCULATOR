1f48638aa14c66dea9916e69e517ed107b062dd9
MADARINHA <n-arlan12@hotmail.com>
2024-02-01 15:54:55 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+import { useState } from 'react';
+import UserInput from './components/UserInput';
+import Result from './components/Result';
+import { calculateInvestmentResults } from './util/investment';
+
+function deriveHandleInputs(userInputs) {
+  let annualResults = calculateInvestmentResults(userInputs);
+  return annualResults;
 }
 
-export default App
+function App() {
+  const [userInputs, setUserInputs] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 120,
+    expectedReturn: 5,
+    duration: 2,
+  });
+  const annualResults = deriveHandleInputs(userInputs);
+  const inputIsValid = userInputs.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    //'+' convert forçadamente para number
+    setUserInputs((prevInputs) => {
+      return {
+        ...prevInputs,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <UserInput userInputs={userInputs} handleChange={handleChange} />;
+      {!inputIsValid && (
+        <p className="center">Please enter valid input data!</p>
+      )}
+      {inputIsValid && <Result annualResults={annualResults} />}
+    </>
+  );
+}
+
+export default App;
